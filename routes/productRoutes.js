687ecdceb1cbd68b93f79949aff91ebda08db611
Noormohammad011 <noormohammad.net1503011@gmail.com>
2022-03-13 02:25:25 +0600

    admin controller is completed

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -2,15 +2,24 @@ import express from 'express'
 import {
   getProductById,
   getProducts,
+  deleteProduct,
+  createProduct,
+  updateProduct,
+  createProductReview,
+  getTopProducts,
 } from '../controllers/productController.js'
+import { protect, admin } from '../middleware/userMiddleware.js'
 
 const router = express.Router()
-
-router.route('/').get(getProducts)
-
+router.route('/').get(getProducts).post(protect, admin, createProduct)
+router.route('/:id/reviews').post(protect, createProductReview)
+router.get('/top', getTopProducts)
 router
   .route('/:id')
   .get(getProductById)
+  .delete(protect, admin, deleteProduct)
+  .put(protect, admin, updateProduct)
+
  
 
 export default router
